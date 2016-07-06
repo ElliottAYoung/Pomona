50b7724bfaf9241879f8adeb9fc7f9a6b21cf8ca
ElliottAYoung <elliott.a.young@gmail.com>
2016-07-06 16:16:53 -0500

    Track Parent on Node

--- lib/pomona/node.rb
+++ lib/pomona/node.rb
@@ -1,11 +1,12 @@
 class Node
-  attr_reader :id
+  attr_reader :id, :parent_id
   attr_accessor :children, :node
 
-  def initialize(data_hash, id)
+  def initialize(data_hash, id, parent_id)
     @id = id
     node_data = { id: @id, children: [] }
     @node = data_hash.merge(node_data)
     @children = @node[:children]
+    @parent_id = parent_id
   end
 end
