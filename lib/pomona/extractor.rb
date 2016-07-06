9bf2900c5a18cde5a385ed3150253f2568af1e84
ElliottAYoung <elliott.a.young@gmail.com>
2016-07-06 14:58:34 -0500

    Get Find by ID working to any level of nesting

--- lib/pomona/extractor.rb
+++ lib/pomona/extractor.rb
@@ -2,23 +2,22 @@ module Extractor
   def self.get_all_by_keys(tree_array = [], keys = [], values = [])
     tree_array.each do |tree_node|
       values << tree_node.node.values_at(*keys)
-      if tree_node.children.any?
-        Extractor.get_all_by_keys(tree_node.children, keys, values)
-      end
+      Extractor.get_all_by_keys(tree_node.children, keys, values) if tree_node.children.any?
     end
 
     values
   end
 
-  def self.find_node_by_id(id, tree_array = [])
+  def self.find_node_by_id(id, tree_array)
+    #TODO: Get this working without using an instance variable for target
     tree_array.each do |tree_node|
       if tree_node.id == id
-        return tree_node
+        @target = tree_node
       else
-        if tree_node.children.any?
-          Extractor.find_node_by_id(id, tree_array)
-        end
+        Extractor.find_node_by_id(id, tree_node.children) if tree_node.children.any?
       end
     end
+
+    @target
   end
 end
