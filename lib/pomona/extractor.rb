789ca51c03c66f684f309ce125967441fc3b124b
ElliottAYoung <elliott.a.young@gmail.com>
2016-07-06 12:14:38 -0500

    Build find by id for node in extractor

--- lib/pomona/extractor.rb
+++ lib/pomona/extractor.rb
@@ -9,4 +9,16 @@ module Extractor
 
     values
   end
+
+  def self.find_node_by_id(id, tree_array = [])
+    tree_array.each do |tree_node|
+      if tree_node.id == id
+        return tree_node
+      else
+        if tree_node.children.any?
+          Extractor.find_node_by_id(id, tree_array)
+        end
+      end
+    end
+  end
 end
